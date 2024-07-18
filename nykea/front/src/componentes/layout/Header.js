27e82e48b5ee3c65d43ddee30bda3bad3e78c0dc
Miqueas Muller <miqueasmuller723@gmail.com>
2024-07-18 16:47:57 -0300

    React parte 1

---
+++ nykea/front/src/componentes/layout/Header.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+
+const Header = (props) => {
+    return (
+        <>
+            <h2>Soy un subtitulo</h2>
+            <header>
+                <img src="" alt=""/>
+                <h2>Soy un subtitulo</h2>
+            </header>
+            <Nav/>
+        </>
+
+    );
+}
+
+export default Header;
---
+++ nykea/front/src/componentes/layout/Nav.js
@@ -0,0 +1,8 @@
+const Nav = (props) => {
+    return (
+        <div>Soy el Nav</div>
+
+    );
+}
+
+export default Nav;

b5e93943193580c4107e9bfa3129b38c0c913c01
Miqueas Muller <miqueasmuller723@gmail.com>
2024-08-18 05:51:11 -0300

    Node BD parte 2

---
+++ nykea/back/models/usuariosModel.js
@@ -0,0 +1,14 @@
+var pool = requiere('./bd');    //llamado de datos BD
+var md5 = requiere('./md5');
+
+async function getUserByUsernameAndPassword(user, password) {
+    try {
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query, [user, md5(password)]);
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { getUserByUsernameAndPassword }
---
+++ nykea/back/routes/admin/novedades.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.render('admin/nvedades', {
+        layout: 'admin/layout',
+        persona: req.session.nombre
+    });    //view/carpetaadmin/login.hbs
+});                                                         //view/carpetaadmin/layout.hbs
+
+module.exports = router;

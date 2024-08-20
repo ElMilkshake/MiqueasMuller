15ea5a4f98c94266cfd83d1708ceede79324a28d
Miqueas Muller <miqueasmuller723@gmail.com>
2024-08-20 06:42:28 -0300

    Agregar novedades

--- nykea/back/routes/admin/novedades.js
+++ nykea/back/routes/admin/novedades.js
@@ -1,11 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel')
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona: req.session.nombre
-    });    //view/carpetaadmin/login.hbs
-});                                                         //view/carpetaadmin/layout.hbs
+        persona: req.session.nombre,
+        novedades
+    });
+});
 
+/*formulario para agregar*/
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {   //agregar hbs
+        layout: 'admin/layout'
+    }) //cierra render
+}); //cierra get
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+
+        console.log(req.body)
+
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
 module.exports = router;

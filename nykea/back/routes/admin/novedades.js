b5153233cdde69ed154974880d2ef08bce0a0cbc
Miqueas Muller <miqueasmuller723@gmail.com>
2024-08-22 17:30:59 -0300

    Imagenes

--- nykea/back/routes/admin/novedades.js
+++ nykea/back/routes/admin/novedades.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel')
+const util = require('util');
+const cloudidary = require('cloudinary').v2;
+const uploader = util.promisify(cloudidary.uploader.upload);
 
 router.get('/', async function (req, res, next) {
 
     var novedades = await novedadesModel.getNovedades();
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+            const imagen = cloudidary.image(novedad.img_id, {
+                width: 100,
+                height: 100,
+                crop: 'fill'
+            });
+            return {
+                ...novedad,
+                imagen
+            }
+        } else {
+            return {
+                ...novedad,
+                imagen: ''
+            }
+        }
+    });
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -23,10 +45,17 @@ router.get('/agregar', (req, res, next) => {
 router.post('/agregar', async (req, res, next) => {
     try {
 
-        console.log(req.body)
+        var img_id = '';
+        if (req.files && Object.keys(req.files).length > 0) {
+            imagen = req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
 
         if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-            await novedadesModel.insertNovedad(req.body);
+            await novedadesModel.insertNovedad({
+                ...req.body,        //spread > titulo, subt, cuerpo
+                img_id
+            });
             res.redirect('/admin/novedades')
         } else {
             res.render('admin/agregar', {
@@ -43,7 +72,7 @@ router.post('/agregar', async (req, res, next) => {
             message: 'No se cargo la novedad'
         })
     }
-})
+});
 
 /*Para eliminar una novedad*/
 router.get('/eliminar/:id', async (req, res, next) => {
@@ -73,7 +102,7 @@ router.get('/modificar/:id', async (req, res, next) => {
 router.post('/modificar', async (req, res, next) => {
     try {
 
-        console.log(id)
+        //console.log(id)
 
         var obj = {
             titulo: req.body.titulo,

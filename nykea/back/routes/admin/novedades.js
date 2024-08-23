46ed22e5a3d83d7019d02274c5ac0430d9a3ffc2
Miqueas Muller <miqueasmuller723@gmail.com>
2024-08-23 20:26:13 -0300

    Crud "3"

--- nykea/back/routes/admin/novedades.js
+++ nykea/back/routes/admin/novedades.js
@@ -4,6 +4,7 @@ var novedadesModel = require('../../models/novedadesModel')
 const util = require('util');
 const cloudidary = require('cloudinary').v2;
 const uploader = util.promisify(cloudidary.uploader.upload);
+const destroy = util.promisify(cloudidary.uploader.destroy);
 
 router.get('/', async function (req, res, next) {
 
@@ -46,6 +47,9 @@ router.post('/agregar', async (req, res, next) => {
     try {
 
         var img_id = '';
+
+        console.log(req.files.imagen);
+
         if (req.files && Object.keys(req.files).length > 0) {
             imagen = req.files.imagen;
             img_id = (await uploader(imagen.tempFilePath)).public_id;
@@ -79,7 +83,10 @@ router.get('/eliminar/:id', async (req, res, next) => {
 
     var id = req.params.id;
 
-    console.log(req.params.id);
+    let novedad = await novedadesModel.getNovedadById(id);
+    if (novedad.img_id) {
+        await (destroy(novedad.img_id));
+    }
 
     await novedadesModel.deleteNovedadesById(id);
     res.redirect('/admin/novedades');
@@ -102,15 +109,28 @@ router.get('/modificar/:id', async (req, res, next) => {
 router.post('/modificar', async (req, res, next) => {
     try {
 
-        //console.log(id)
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+        } else {
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+            }
+        }
+        if (borrar_img_vieja && req.body.img_original) {
+            await (destroy(req.body.img_original));
+        }
 
         var obj = {
             titulo: req.body.titulo,
             subtitulo: req.body.subtitulo,
             cuerpo: req.body.cuerpo
         }
-
-        console.log(req.body.id)
 
         await novedadesModel.modificarNovedadById(obj, req.body.id);
         res.redirect('/admin/novedades');
@@ -124,4 +144,4 @@ router.post('/modificar', async (req, res, next) => {
         })
     }
 })
-    module.exports = router;
+module.exports = router;
